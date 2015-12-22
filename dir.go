a99a413b21a184d14c15d849b4b8abb849ea701b
xiaobai <xiaobai@asyou.me>
2015-12-22 15:32:37 +0800

    first commit

---
+++ dir.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
+
+func GetDirs(path string) (*[]string, error) {
+	fi, err := os.Open(path + "/.inotify")
+	dir := make([]string, 0, 30)
+	config_dirs := make([]string, 0, 30)
+	if err != nil {
+		return &dir, err
+	}
+	defer fi.Close()
+	fd, err := ioutil.ReadAll(fi)
+	if err != nil {
+		return &dir, err
+	}
+
+	if err = json.Unmarshal(fd, &config_dirs); err != nil {
+		if err != nil {
+			return &dir, err
+		}
+	}
+	for _, v := range config_dirs {
+		WalkDir(path+"/"+v, &dir)
+	}
+
+	dir = append(dir, "")
+	dir = append(dir, config_dirs...)
+
+	return &dir, nil
+}
+
+//获取指定目录及所有子目录下的所有文件，可以匹配后缀过滤。
+func WalkDir(dirPth string, dirs *[]string) (err error) { //忽略后缀匹配的大小写
+	dir_real := *dirs
+	err = filepath.Walk(dirPth, func(filename string, fi os.FileInfo, err error) error { //遍历目录
+
+		if fi.IsDir() {
+			dir_real = append(dir_real, fi.Name())
+		}
+		return nil
+	})
+	return err
+}
---
+++ main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"gopkg.in/fsnotify.v1"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
+)
+
+var done = make(chan bool)
+var cmd_chan = make(chan *exec.Cmd)
+
+func Usage() {
+	fmt.Fprint(os.Stderr, "Usage of ", os.Args[0], ":\n")
+	flag.PrintDefaults()
+	fmt.Fprint(os.Stderr, "\n")
+}
+
+func main() {
+	path := flag.String("path", "", "path like /tmp")
+	shell := flag.String("shell", "", "a shell file")
+
+	flag.Parse()
+
+	*path = strings.TrimRight(*path, "/")
+
+	dirs, err := GetDirs(*path)
+	if err != nil {
+		Usage()
+		panic(err)
+	}
+
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		Usage()
+		panic(err)
+	}
+	defer watcher.Close()
+
+	go func() {
+		var last_time int64 = 0
+		var run_num int64 = 0
+		var run_cmd *exec.Cmd
+		go run(shell)
+		for {
+			select {
+			case _ = <-watcher.Events:
+				run_num++
+			case err := <-watcher.Errors:
+				fmt.Println("error:", err)
+			case run_cmd = <-cmd_chan:
+
+			}
+			if run_num > 10 || (run_num > 0 && time.Now().Unix() > last_time+5) {
+				run_cmd.Process.Kill()
+				run_num = 0
+				last_time = time.Now().Unix()
+				go run(shell)
+			}
+		}
+	}()
+
+	for _, v := range *dirs {
+		err = watcher.Add(*path + "/" + v)
+		if err != nil {
+			Usage()
+			panic(err)
+		}
+	}
+
+	<-done
+}
+
+func run(shell *string) {
+	cmd := exec.Command("sh", *shell)
+	cmdReader, err := cmd.StdoutPipe()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error creating StdoutPipe for Cmd", err)
+	}
+
+	scanner := bufio.NewScanner(cmdReader)
+	go func() {
+		for scanner.Scan() {
+			fmt.Printf("%s\n", scanner.Text())
+		}
+	}()
+
+	cmd_chan <- cmd
+	cmd.Start()
+	cmd.Wait()
+}
